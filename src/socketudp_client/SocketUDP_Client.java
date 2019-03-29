560b6da66c522a1f022dfc29dedf74bcdcdd9113
lleal24 <lorenaleal24@gmail.com>
2019-03-29 03:01:53 -0500

    Se agregan reglas en la conversion del array a binario linreria Array.toString, se eliminan las comas, espacios en blanco, corchete de apertura y cierre ya que estan generango inconveniente en el lado del servidor al convertir de String a Array incluye las llaves por lo que no es posible realizar la operacion de multiplicacion

--- src/socketudp_client/SocketUDP_Client.java
+++ src/socketudp_client/SocketUDP_Client.java
@@ -28,41 +28,46 @@ public class SocketUDP_Client {
      */
     public static void main(String[] args) {
         try {
-            // TODO code application logic here
-            String msj = JOptionPane.showInputDialog("Ingrese el mensaje a enviar");
+            // TODO code application logic here           
             DatagramSocket miSocket = new DatagramSocket();
             //conversion de string msj a bytes para enviar
-            byte [] mensaje = msj.getBytes();
             InetAddress host = InetAddress.getByName("127.0.0.1");
             //puerto de destino definido en el servidor
             int puerto = 9107;
-                
-             Scanner entrada = new Scanner(System.in);
-             int [] binario = new int [7];
-             
-             
-             for(int i=binario.length-1; i>=0; i--){
-                 System.out.print("Ingrese numero");
-                 binario[i] = entrada.nextInt();
-             }
-                //Conversion de array a estring separado por comas
-                String str = Arrays.toString(binario);
-                System.out.print(str);
-                
-                /*Impresion de array
-               for(int i=0; i<binario.length; i++){
-                System.out.println(binario[i]);
-                 }
-                */
-            byte [] mensaje2 = str.getBytes();    
-             
-             
-            //Se arma el paquete a enviar que contiene mensaje host y puerto
-            DatagramPacket miPaquete = new DatagramPacket(mensaje, msj.length(), host, puerto);
-            miSocket.send(miPaquete);
-             DatagramPacket miPaquete2 = new DatagramPacket(mensaje2, str.length(), host, puerto);
-            miSocket.send(miPaquete2);
+
+            //metodo de entrada teclado
+            Scanner entrada = new Scanner(System.in);
+            //array de 7 posiciones para guardar binario
+            int[] binario = new int[7];
+
+            //ciclo para guardar elementos, llena de forma inversa
+            for (int i = binario.length - 1; i >= 0; i--) {
+                System.out.print("Ingrese numero");
+                binario[i] = entrada.nextInt();
+            }
+
+            //Conversion de array a string separado por comas
+            String str = Arrays.toString(binario)          
+            .replace(",", "")  //remove the commas
+            .replace("[", "")  //remove the right bracket
+            .replace("]", "")  //remove the left bracket
+            .trim();    
             
+            
+            System.out.print(str);
+
+            /*Impresion de array
+             for(int i=0; i<binario.length; i++){
+             System.out.println(binario[i]);
+             }
+             */
+            byte[] mensaje = str.getBytes();
+
+            //Se arma el paquete a enviar que contiene mensaje host y puerto
+            DatagramPacket miPaquete = new DatagramPacket(mensaje, str.length(), host, puerto);
+            miSocket.send(miPaquete);
+          
+
         } catch (SocketException ex) {
             Logger.getLogger(SocketUDP_Client.class.getName()).log(Level.SEVERE, null, ex);
         } catch (UnknownHostException ex) {
@@ -71,5 +76,5 @@ public class SocketUDP_Client {
             Logger.getLogger(SocketUDP_Client.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
 }
