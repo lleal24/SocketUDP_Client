a46303eb0cd9c841f8140c0ceaeb9c6eea258ea2
lleal24 <lorenaleal24@gmail.com>
2019-03-31 19:37:03 -0500

    Inicialmente se estaba enviando un solo mensaje String como vector hacia el servidor, en este cambio se envian los bites opciones 1 o 0 uno a uno es decir envia 7 datos al final hacia el servidor, con los condicionales anidados se controla el numero de veces a solicitar opcion y mostrar tarjetas cuanto el contador sea igual a 7 septimo datos este debe finalizar mostrando el numero adivinado que envia el servidor.

--- src/socketudp_client/SocketUDP_Client.java
+++ src/socketudp_client/SocketUDP_Client.java
@@ -34,18 +34,24 @@ public class SocketUDP_Client {
             InetAddress host = InetAddress.getByName("127.0.0.1");
             //puerto de destino definido en el servidor
             int puerto = 9107;
+            String cadena = "";
 
             //metodo de entrada teclado
             Scanner entrada = new Scanner(System.in);
             //array de 7 posiciones para guardar binario
-            int[] binario = new int[7];
+           // int[] binario = new int[7];
 
-            //ciclo para guardar elementos, llena de forma inversa
+            for(int i=0; i<7;i++){
+               System.out.print("Ingrese numero");
+               cadena = entrada.next();
+           }
+            /*ciclo para guardar elementos, llena de forma inversa
             for (int i = binario.length - 1; i >= 0; i--) {
                 System.out.print("Ingrese numero");
                 binario[i] = entrada.nextInt();
             }
-
+            
+           
             //Conversion de array a string separado por comas
             String str = Arrays.toString(binario)          
             .replace(",", "")  //remove the commas
@@ -53,19 +59,19 @@ public class SocketUDP_Client {
             .replace("[", "")  //remove the right bracket
             .replace("]", "")  //remove the left bracket
             .trim();    
+            */
             
-            
-            System.out.print(str);
+            System.out.print(cadena);
 
             /*Impresion de array
              for(int i=0; i<binario.length; i++){
              System.out.println(binario[i]);
              }
              */
-            byte[] mensaje = str.getBytes();
+            byte[] mensaje = cadena.getBytes();
 
             //Se arma el paquete a enviar que contiene mensaje host y puerto
-            DatagramPacket miPaquete = new DatagramPacket(mensaje, str.length(), host, puerto);
+            DatagramPacket miPaquete = new DatagramPacket(mensaje, cadena.length(), host, puerto);
             miSocket.send(miPaquete);
           
 
